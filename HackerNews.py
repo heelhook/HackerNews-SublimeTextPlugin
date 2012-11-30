c809b62a2e6d01ef7901ede86ce667d5ac6bf7ba
Tim Davies <mail@timdavi.es>
2012-11-30 17:32:08 +0000

    Can now click on stories to open in your browser :-D

--- HackerNews.py
+++ HackerNews.py
@@ -6,6 +6,48 @@ import sublime_plugin
 import os.path
 import json
 from hn import HackerNews
+import webbrowser
+import re
+
+
+HN_TMLANGUAGE_PATH = 'Packages/HackerNews/HackerNews.tmLanguage'
+URL_CACHE = {}
+
+
+class DragSelectCallbackCommand(sublime_plugin.TextCommand):
+    def run_(self, args):
+        for c in sublime_plugin.all_callbacks.setdefault('on_pre_mouse_down',[]):
+            c.on_pre_mouse_down(args, self.view)
+
+        #We have to make a copy of the selection, otherwise we'll just have
+        #a *reference* to the selection which is useless if we're trying to
+        #roll back to a previous one. A RegionSet doesn't support slicing so
+        #we have a comprehension instead.
+        old_sel = [r for r in self.view.sel()]
+
+        #Only send the event so we don't do an extend or subtract or
+        #whatever. We want the only selection to be where they clicked.
+        self.view.run_command("drag_select", {'event': args['event']})
+        new_sel = self.view.sel()
+        click_point = new_sel[0].a
+
+        #Restore the old selection so when we call drag_select in will
+        #behave normally.
+        new_sel.clear()
+        map(new_sel.add, old_sel)
+
+        #This is the "real" drag_select that alters the selection for real.
+        self.view.run_command("drag_select", args)
+
+        for c in sublime_plugin.all_callbacks.setdefault('on_post_mouse_down',[]):
+            c.on_post_mouse_down(click_point, self.view)
+
+class MouseEventListener(sublime_plugin.EventListener):
+    sublime_plugin.all_callbacks.setdefault('on_pre_mouse_down', [])
+    sublime_plugin.all_callbacks.setdefault('on_post_mouse_down', [])
+    
+    def __init__(self, view = None):
+        self.current_view = view
 
 
 class OpenHackerNewsCommand(sublime_plugin.WindowCommand):
@@ -41,6 +83,9 @@ class OpenHackerNewsCommand(sublime_plugin.WindowCommand):
             # Figure out indentation for next line:
             line_indent = len(line)
             
+            # Add URL to cache:
+            URL_CACHE[story['title']] = story['url']
+            
             # Add in headline:
             line += "%s" % (story['title'])
             
@@ -66,3 +111,20 @@ class OpenHackerNewsCommand(sublime_plugin.WindowCommand):
         view.end_edit(edit)
         view.set_scratch(True)
         view.set_name('Hacker News')
+
+class MouseEventProcessor(MouseEventListener):
+    def on_post_mouse_down(self, point, view):
+        if view.settings().get('syntax') != HN_TMLANGUAGE_PATH:
+            return
+        
+        region = view.line(point)
+        line = view.substr(region)
+        
+        if re.match('^\(([\d]*)\)([ ]{1,10})(.*)$', line):
+            title = re.sub('^\(([\d]*)\)([ ]{1,10})', '', line)
+            
+            try:
+                url = URL_CACHE[title]
+                webbrowser.open(url)
+            except:
+                print "HackerNews: couldn't figure out URL"
