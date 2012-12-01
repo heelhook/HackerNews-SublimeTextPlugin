13822c4493e1d4815ccfd5f8611377d9f945f4f3
Tim Davies <mail@timdavi.es>
2012-12-01 20:57:44 +0000

    Changed API to point at my hosted cache to improve speed and reliability

--- hn.py
+++ hn.py
@@ -4,15 +4,10 @@ import urllib2
 
 
 class HackerNews (object):
-    def _api_call (self, path):
-        url = 'http://api.ihackernews.com' + path
+    def get_stories (self):
+        url = 'http://timdavi.es/hackernews/'
         data = urllib2.urlopen(url).read()
         return json.loads(data)
-    
-    def get_stories (self):
-        data = self._api_call('/page?format=json')
-        return data
-
 
 if __name__ == '__main__':
     a = HackerNews()
