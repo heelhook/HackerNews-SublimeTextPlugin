c69911367064e03413b5f70087859ff74c21223f
Tim Davies <mail@timdavi.es>
2012-12-02 03:09:32 +0000

    Separated mouse event code up

--- HackerNews.py
+++ HackerNews.py
@@ -8,46 +8,11 @@ import json
 from hn import HackerNews
 import webbrowser
 import re
+from MouseEvents import DragSelectCallbackCommand, MouseEventListener
 
 
 HN_TMLANGUAGE_PATH = 'Packages/HackerNews/HackerNews.tmLanguage'
 URL_CACHE = {}
-
-
-class DragSelectCallbackCommand(sublime_plugin.TextCommand):
-    def run_(self, args):
-        for c in sublime_plugin.all_callbacks.setdefault('on_pre_mouse_down',[]):
-            c.on_pre_mouse_down(args, self.view)
-
-        #We have to make a copy of the selection, otherwise we'll just have
-        #a *reference* to the selection which is useless if we're trying to
-        #roll back to a previous one. A RegionSet doesn't support slicing so
-        #we have a comprehension instead.
-        old_sel = [r for r in self.view.sel()]
-
-        #Only send the event so we don't do an extend or subtract or
-        #whatever. We want the only selection to be where they clicked.
-        self.view.run_command("drag_select", {'event': args['event']})
-        new_sel = self.view.sel()
-        click_point = new_sel[0].a
-
-        #Restore the old selection so when we call drag_select in will
-        #behave normally.
-        new_sel.clear()
-        map(new_sel.add, old_sel)
-
-        #This is the "real" drag_select that alters the selection for real.
-        self.view.run_command("drag_select", args)
-
-        for c in sublime_plugin.all_callbacks.setdefault('on_post_mouse_down',[]):
-            c.on_post_mouse_down(click_point, self.view)
-
-class MouseEventListener(sublime_plugin.EventListener):
-    sublime_plugin.all_callbacks.setdefault('on_pre_mouse_down', [])
-    sublime_plugin.all_callbacks.setdefault('on_post_mouse_down', [])
-    
-    def __init__(self, view = None):
-        self.current_view = view
 
 
 class OpenHackerNewsCommand(sublime_plugin.WindowCommand):
@@ -115,6 +80,7 @@ class OpenHackerNewsCommand(sublime_plugin.WindowCommand):
         view.set_scratch(True)
         view.set_name('Hacker News')
 
+
 class MouseEventProcessor(MouseEventListener):
     def on_post_mouse_down(self, point, view):
         if view.settings().get('syntax') != HN_TMLANGUAGE_PATH:
@@ -137,6 +103,7 @@ class MouseEventProcessor(MouseEventListener):
                 webbrowser.open(url)
             except:
                 print "HackerNews: couldn't figure out URL"
+        
         elif re.match('^([ ]{1,10})Uploaded by:', line):
             word = view.substr(view.word(point)).strip()
             
